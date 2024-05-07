f304dcd034c699fa989e73d3f834d44753a64c21
selvin-rivera <nrivera.clements@gmail.com>
2024-05-06 20:42:15 -0600

    parametro

--- EjercicioAbstraccion/Atributos.cs
+++ EjercicioAbstraccion/Atributos.cs
@@ -9,33 +9,33 @@ namespace EjercicioAbstraccion
         public double N1 { get; set; }
         public double N2 { get; set; }
 
-        public override double Division()
+        public override double Division(double N1, double N2)
  
         {
             return N1 / N2;
         }
 
-        public override double Multiplicacion()
+        public override double Multiplicacion(double N1, double N2)
         {
             return N1 * N2;
         }
 
-        public override double Resta()
+        public override double Resta(double N1, double N2)
         {
             return N1 - 2;
         }
 
-        public override double Suma()
+        public override double Suma(double N1, double N2)
         {
             return N1 + N2;
         }
 
         public void Imprimir ()
         {
-            Console.WriteLine($"La suma es : {Suma()}");
-            Console.WriteLine($"La resta es;  {Resta()}");
-            Console.WriteLine($"La multiplicacion es : {Multiplicacion()}");
-            Console.WriteLine($"La Division es : {Division()}");
+            Console.WriteLine($"La suma es : {Suma(N1, N2)}");
+            Console.WriteLine($"La resta es;  {Resta(N1, N2)}");
+            Console.WriteLine($"La multiplicacion es : {Multiplicacion(N1, N2)}");
+            Console.WriteLine($"La Division es : {Division(N1, N2)}");
         }
     }
 
--- EjercicioAbstraccion/Operacion.cs
+++ EjercicioAbstraccion/Operacion.cs
@@ -4,9 +4,9 @@ namespace EjercicioAbstraccion
 {
     public abstract class Operacion
     {
-        public abstract double Suma();
-        public abstract double Resta();
-        public abstract double Multiplicacion();
-        public abstract double Division();
+        public abstract double Suma(double N1, double N2);
+        public abstract double Resta(double N1, double N2);
+        public abstract double Multiplicacion(double N1, double N2);
+        public abstract double Division(double N1, double N2);
     }
 }
